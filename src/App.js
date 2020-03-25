ca7865926718b6b33113975cc74cea5905110efb
adri51 <adriibm91@gmail.com>
2020-03-25 13:00:01 +0100

    buscador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Title } from './components/Title';
+import { SearchForm } from './components/SearchForm';
+
 import './App.css';
 import 'bulma/css/bulma.css';
+
 
 function App() {
   return (
     <div className="App">
-      Search Movies
+      <Title>Search Movies</Title>
+      <div className="Searchform-wrapper">
+        <SearchForm />
+      </div>
     </div>
   );
-}
+};
 
 export default App;

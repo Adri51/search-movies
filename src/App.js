415dc31f216745fdfbefe1b08ab237fca5c488de
adri51 <adriibm91@gmail.com>
2020-03-26 16:57:25 +0100

    api pelis

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
+import { MovieList } from './components/MoviesList';
 
 import './App.css';
 import 'bulma/css/bulma.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Search Movies</Title>
-      <div className="Searchform-wrapper">
-        <SearchForm />
+
+class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  };
+
+  // _renderResults() {
+  //   const { results } = this.state
+  //   return results.map(movie => {
+  //     return (
+  //       <Movie
+  //         key={movie.imdbID}
+  //         title={movie.title}
+  //         year={movie.year}
+  //         poster={movie.poster}
+  //       />
+  //     )
+  //   })
+  // }
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="Searchform-wrapper">
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin resultados </p>
+          : <MovieList movies={this.state.results} />
+          // : this._renderResults()
+        }
+
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const API_KEY = '4287ad07';
+
 export class SearchForm extends Component {
     state = {
         inputMovie: ''
@@ -10,7 +12,17 @@ export class SearchForm extends Component {
 
     _handleSubmit = (e) => {
         e.preventDefault()
-        alert(this.state.inputMovie)
+        // alert(this.state.inputMovie)
+        const { inputMovie } = this.state
+
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+            .then(res => res.json())
+            .then(results => {
+                // console.log(results)
+                const { Search, totalResults } = results
+                console.log({ Search, totalResults })
+                this.props.onResults(Search)
+            })
     }
     render() {
         return (
